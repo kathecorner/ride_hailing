eaab4320049fcfaee3cfb65cc764caf277583c8c
kathecorner <40414604+kathecorner@users.noreply.github.com>
2022-10-11 11:33:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -88,6 +88,28 @@ app.post("/api/sessions", async (req, res) => {
       postalCode : "984 99",
       stateOrProvince : "Stockholm"
    },
+      billingAddress : {
+      city : "Bodens kommun",
+      country : "SE",
+      street : "110 Ferne Spurs",
+      houseNumberOrName : "96146, Norrbotten County",
+      postalCode : "984 99",
+      stateOrProvince : "Stockholm"
+   },
+   deliveryDate : "2022-05-29T12:30:02Z",   
+   browserInfo : {
+      userAgent : "Mozilla\/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit\/605.1.15 (KHTML, like Gecko) Version\/14.1.1 Mobile\/15E148 Safari\/604.1",
+      acceptHeader : "application\/json",
+      language : "en-ca",
+      colorDepth : 32,
+      screenHeight : 896,
+      screenWidth : 414,
+      timeZoneOffset : -330,
+      javaEnabled : false
+   },   
+   accountInfo : {
+      accountCreationDate : "2022-05-25T06:46:41Z"
+   },
       
       
       //countryCode: "US",
